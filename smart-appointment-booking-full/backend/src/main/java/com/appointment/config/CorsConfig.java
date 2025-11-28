00f7e2670bf3c316d998ac12bee4e62ed571256d
I585796 <nilay.shekhar@sap.com>
2025-11-28 05:42:56 +0530

    change url path

--- smart-appointment-booking-full/backend/src/main/java/com/appointment/config/CorsConfig.java
+++ smart-appointment-booking-full/backend/src/main/java/com/appointment/config/CorsConfig.java
@@ -13,7 +13,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000", "http://18.61.253.86:3000")
+            .allowedOrigins(
+                "http://localhost:3000",
+                "http://18.61.253.86:3000",
+                "http://18.61.253.86:8080"
+            )
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
             .allowedHeaders("*")
             .allowCredentials(true);
